d9681830598b6d7bd70c21e36bc22c270bcc7cba
MasterVitte <vitte.st13@gmail.com>
2024-04-28 15:10:50 +0300

    feat: add register page, logo, translations TDT-8

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { LoginPage } from './widgets/Login';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { RegisterPage } from './widgets/Register';
 
-const defaultTheme = createTheme();
+const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: 'rgba(125,0,249,0.85)',
+    },
+  },
+});
 
 export function App() {
   return (
@@ -11,8 +18,11 @@ export function App() {
       <Router>
         <div>
           <Switch>
-            <Route path="/login" component={LoginPage} />
-            <Route path="/">home</Route>
+            <Route path="/">
+              <Route path="/login" component={LoginPage} />
+              <Route path="/register" component={RegisterPage} />
+              home
+            </Route>
           </Switch>
         </div>
       </Router>
---
+++ src/app/shared/Logo/ui/Logo.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const Logo = () => {
+  return (
+    <img src="../../../../assets/logo.png" alt="logo" width={60} height={60} />
+  );
+};
