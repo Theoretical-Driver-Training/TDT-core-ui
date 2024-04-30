1430a03744d7db503f35f905e461bf8647bfa261
MasterVitte <vitte.st13@gmail.com>
2024-04-30 18:06:09 +0300

    feat: history widget TDT-23

---
+++ src/app/shared/Sidebar/ui/HistoryWidget/ui/HistoryItem.tsx
@@ -0,0 +1,43 @@
+import { Grid, ListItem, Typography } from '@mui/material';
+import React from 'react';
+import { SvgIconComponent } from '@mui/icons-material';
+
+interface Props {
+  label: string;
+  date?: string;
+  IconComponent: SvgIconComponent;
+  iconColor?: string;
+  endAdornment?: React.ReactNode;
+}
+
+export const HistoryItem = ({
+  label,
+  date,
+  IconComponent,
+  iconColor,
+  endAdornment,
+}: Props) => {
+  return (
+    <ListItem disablePadding>
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="center"
+        paddingTop={2}
+      >
+        <Grid container item gap={2} xs={8} wrap="nowrap" alignItems="center">
+          <IconComponent
+            style={{ width: '40px', height: '40px', color: iconColor }}
+          />
+          <Grid>
+            <Typography whiteSpace="nowrap">{label}</Typography>
+            <Typography whiteSpace="nowrap" color="#aaa" variant="subtitle2">
+              {date}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item>{endAdornment}</Grid>
+      </Grid>
+    </ListItem>
+  );
+};
