d181509ab7ac3e1409b619d8932167d8b0260a38
MasterVitte <vitte.st13@gmail.com>
2024-04-30 15:09:46 +0300

    fix: dynamic breadcrumbs, new design of breadcrumbs TDT-20

--- src/app/shared/Layout/ui/Layout.tsx
+++ src/app/shared/Layout/ui/Layout.tsx
@@ -8,9 +8,14 @@ import {
 } from '@mui/material';
 import { Sidebar } from '../../Sidebar';
 import { AppToolbar } from '../../AppToolbar';
+import { BreadCrumbsToolbar } from './BreadCrumbs';
+
+const BreadCrumbsWrapper = styled(Grid)<GridProps>(({ theme }) => ({
+  padding: theme.spacing(3, 2),
+}));
 
 const GridLayout = styled(Grid)<GridProps>(({ theme }) => ({
-  padding: theme.spacing(2, 2),
+  padding: theme.spacing(0, 2, 2),
 }));
 
 export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
@@ -20,6 +25,9 @@ export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <>
       <AppToolbar />
+      <BreadCrumbsWrapper container>
+        <BreadCrumbsToolbar />
+      </BreadCrumbsWrapper>
       <GridLayout container gap={3} wrap="nowrap">
         <Grid item lg={8} md={11} xs={11}>
           {children}
--- src/app/shared/Router/ui/AppRouter.tsx
+++ src/app/shared/Router/ui/AppRouter.tsx
@@ -5,38 +5,25 @@ import { RegisterPage } from '../../../widgets/Register';
 import { ProfilePage } from '../../../widgets/Profile';
 import React from 'react';
 import { HomePage } from '../../../widgets/Home';
-
-export const APP_ROUTES = {
-  HOME: '/',
-  PROFILE: '/profile',
-};
-
-export const ROUTES = {
-  LOGIN: '/login',
-  REGISTER: '/register',
-  ...APP_ROUTES,
-} as const;
-
-export const ROUTE_NAMES = {
-  [ROUTES.HOME]: 'Главная',
-  [ROUTES.PROFILE]: 'Профиль',
-} as const;
-
-export const BASE_ROUTE = ROUTES.HOME;
+import { ROUTES } from '../lib';
 
 export const AppRouter = () => {
   return (
     <Router>
       <div>
         <Switch>
-          <LayoutRoute path={ROUTES.HOME} exact>
+          <LayoutRoute path={ROUTES.HOME.path} exact>
             <HomePage />
           </LayoutRoute>
-          <Route path={ROUTES.LOGIN} component={LoginPage} />
-          <Route path={ROUTES.REGISTER} component={RegisterPage} />
-          <LayoutRoute path={ROUTES.PROFILE}>
+          <Route path={ROUTES.LOGIN.path} component={LoginPage} />
+          <Route path={ROUTES.REGISTER.path} component={RegisterPage} />
+          <LayoutRoute path={ROUTES.PROFILE.path}>
             <ProfilePage />
           </LayoutRoute>
+          <LayoutRoute path="/tests" exact>
+            testhome
+          </LayoutRoute>
+          <LayoutRoute path="/test/:testId">test</LayoutRoute>
         </Switch>
       </div>
     </Router>
