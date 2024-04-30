01976b7cb0877b916433418be712eb54d8ad3d10
MasterVitte <vitte.st13@gmail.com>
2024-04-30 20:27:10 +0300

    feat: add statistics widget TDT-27

--- src/app/widgets/Home/ui/HomePage.tsx
+++ src/app/widgets/Home/ui/HomePage.tsx
@@ -1,15 +1,25 @@
-import { Grid } from '@mui/material';
+import { Grid, useMediaQuery } from '@mui/material';
 import { WelcomeWidget } from './WelcomeWidget';
 import { ResultsWidget } from './ResultsWidget';
+import { ActivityWidget } from './ActivityWidget';
+import { useTheme } from '@mui/material/styles';
 
 export const HomePage = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
   return (
     <Grid container xs={12} gap={3}>
       <Grid xs={12}>
         <WelcomeWidget />
       </Grid>
-      <Grid xs={12} sm={12} md={5} lg={5}>
-        <ResultsWidget />
+      <Grid container xs={12} gap={3} wrap={isMobile ? 'wrap' : 'nowrap'}>
+        <Grid item xs={12} sm={12} md={5} lg={5}>
+          <ResultsWidget />
+        </Grid>
+        <Grid item xs={12} sm={12} md={7} lg={7}>
+          <ActivityWidget />
+        </Grid>
       </Grid>
     </Grid>
   );
